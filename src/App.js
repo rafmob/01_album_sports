f6c466a1d8ffa7d6bd706484d77382db90e41eec
rafmob <89050871+rafmob@users.noreply.github.com>
2022-08-04 21:19:09 -0300

    Criação dos cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import './App.css';
+import Card from './components/Card';
 import Header from './components/Header';
-import SearchAdd from './components/SearchAdd'
+import SearchAdd from './components/SearchAdd';
+
+const Cards = [
+  {
+      id: 1,
+      nome: "Neymar Jr.",
+      numeroCamisa: 10,
+      idade: 30,
+      time: 'Paris Saint-Germain',
+      imagem: "https://pbs.twimg.com/media/E54lvMJXEAIjKVI.jpg:large"
+  },
+  {
+    id: 2,
+    nome: "Neymar Jr.",
+    numeroCamisa: 10,
+    idade: 30,
+    time: 'Paris Saint-Germain',
+    imagem: "https://pbs.twimg.com/media/E54lvMJXEAIjKVI.jpg:large"
+  },
+  {
+    id: 3,
+    nome: "Neymar Jr.",
+    numeroCamisa: 10,
+    idade: 30,
+    time: 'Paris Saint-Germain',
+    imagem: "https://cdn.ome.lt/iPC6g7VegtasIJdu6I3RtBK_7eA=/770x0/smart/uploads/conteudo/fotos/Neymar_camisa_Marilia_Mendonca.png"
+  },
+  {
+    id: 4,
+    nome: "Neymar Jr.",
+    numeroCamisa: 10,
+    idade: 30,
+    time: 'Paris Saint-Germain',
+    imagem: "https://cdn.ome.lt/iPC6g7VegtasIJdu6I3RtBK_7eA=/770x0/smart/uploads/conteudo/fotos/Neymar_camisa_Marilia_Mendonca.png"
+  },
+]
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchAdd />
+      <div className='allCards'>
+        {Cards.map(card => <Card card={card} />)}
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import '../App.css';
+
+export default function Card(props) {
+
+    const { nome, numeroCamisa, idade, time, imagem } = props.card;
+
+    return (
+        <div>
+            <div class="card">
+                <div className='bgImage'>
+                    <img src={imagem} alt="" />
+                </div>
+                <div className='infoCard'>
+                    <div>
+                        <h2>{nome} - {numeroCamisa}</h2>
+                    </div>
+                    <div>
+                        {time} 
+                    </div>
+                    <div>
+                        {idade} ANOS                
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
