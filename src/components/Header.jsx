429ab7a3771b91db7fece46203b16de82dbc4142
rafmob <89050871+rafmob@users.noreply.github.com>
2022-09-17 11:01:10 -0300

    Alteração de card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import '../App.css';
+import SearchAdd from './SearchAdd';
 import logo from '../img/logo.svg'
 
 function Header() {
   return (
-    <div className="Header">
-      <img src={logo} alt="Logotipo Album Sport" />
+    <>
+      <div className="Header">
+        <img src={logo} alt="Logotipo Album Sport" />
+      </div>
       
-    </div>
+      <SearchAdd />
+    </>
   );
 }
 
